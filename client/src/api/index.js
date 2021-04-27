4988d6c68d1b618c3df940c0006d9872a8eb5de0
haopeng jiang <haopengjiang4work@gmail.com>
2021-04-27 18:51:53 -0400

    add Procfile

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
- const API = axios.create({ baseURL: 'http://localhost:5000' });
- // const API = axios.create({ baseURL: 'https://haopengjiang-mern-project-backend.zeet.app' });
+ // const API = axios.create({ baseURL: 'http://localhost:5000' });
+  const API = axios.create({ baseURL: 'https://mern-project-memory-backend.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
